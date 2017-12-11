b8a970abef97fb9e22ab2f8a4c83735ee5aa658f
Chris Hakos <chrishakos@gmail.com>
2017-12-11 15:32:51 -0500

    draw out transaction logic

--- cart.js
+++ cart.js
@@ -1,9 +1,29 @@
-function Cart(){
-  var obj = {
-    cart: [],
-    total: 0,
-    discount: 0
-  };
+class Transaction{
+
+    constructor(inventory){
+      this.inventory = inventory;
+      this.cart = {};
+      this.subTotal = 0;
+      this.discounts = 0;
+      this.total = 0;
+    }
+
+    scanItem(item){
+      if (this.inventory.itemCount[item] > 0 && this.cart[item]){
+        this.cart[item] += 1;
+      } else {
+        this.cart[item] = 1;
+      }
+      this.cart.total += this.inventory.prices[item];
+    }
+
+    purchase(){
+      this.inventory.cart.forEach((item) => {
+        this.inventory.itemCount[item] -= 1;
+      });
+    }
+
+
 }
 
 
@@ -14,3 +34,8 @@ const addItem = () => {
 const calculateDiscount = () => {
 
 };
+
+
+var storeOneInv = new Inventory(items);
+
+var transaction1 = new Transaction(storeOneInv);
